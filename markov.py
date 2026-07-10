650fbfdf34ba18ecde5c36206f5d19e9f7d53643
Laurent Cimon <Laurent.Cimon.1@ulaval.ca>
2026-07-10 15:00:45 -0400

    Ajouter la chaine de markov

---
+++ markov.py
@@ -0,0 +1,61 @@
+import json
+import random
+
+class Markov:
+    def __init__(self):
+        self.historique = {}
+        self.ignorer = '.,:;-\r\n'
+
+    def load(self, chemin):
+        with open(chemin, 'r') as f:
+            json.load(chemin)
+
+    def save(self, chemin):
+        with open(chemin, 'w') as f:
+            json.dump(self.historique, f)
+
+    def ajouter_a_historique(self, mots, mot_a_ajouter):
+        if mots in self.historique:
+            self.historique[mots].append(mot_a_ajouter)
+        else:
+            self.historique[mots] = [mot_a_ajouter]
+
+    def lire_fichier(self, chemin):
+        with open(chemin, 'r', encoding='utf8') as f:
+            self.lire(f.read())
+
+    def nettoyer_texte(self, texte):
+        texte = texte.replace('\n\n', ' ')
+        texte = texte.replace('\r\n\r\n', ' ')
+        texte = texte.replace('\n', ' ')
+        texte = texte.replace('\r\n', ' ')
+        texte = texte.lower()
+        return texte
+
+    def lire(self, texte):
+        texte = self.nettoyer_texte(texte)
+        mots = ('', '')
+        for mot in texte.split(' '):
+            mot_propre = mot.strip(self.ignorer)
+            self.ajouter_a_historique(mots, mot_propre)
+
+            mots = (mots[1], mot_propre)
+
+            if '.' in mot:
+                self.ajouter_a_historique(mots, '.')
+                mots = ('', '')
+
+    def retirer_mot(self, mots):
+        return random.choice(self.historique[mots])
+
+    def generer_phrase(self, debut=None):
+        mots = ['', ''] if debut is None else ['', debut]
+        arreter = False
+        while not arreter:
+            mot = self.retirer_mot((mots[-2], mots[-1]))
+            if mot == '.':
+                arreter = True
+            else:
+                mots.append(mot)
+
+        return ' '.join([mot for mot in mots if mot != '']) + '.'
